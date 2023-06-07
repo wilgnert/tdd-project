ed8218677debb1bd370b86724d3b06df8dffeb6d
Wilgnert de Alcântara <wilgnert@hotmail.com>
2023-06-07 14:52:34 -0300

    Implementando o Novo Projeto (Listas e Items) - PARTE 5

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -82,7 +82,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Agora, um novo usuário, João, entra no site
         self.browser.quit()
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.Firefox(options=options)
 
         # João visita a página inicial. Não existe nenhum sinal da lista de Maria
         self.browser.get(self.live_server_url)
